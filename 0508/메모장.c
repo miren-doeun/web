33043868fc3e40f83be9ff82993bdee784d4b1b9
miren-doeun <111549068+miren-doeun@users.noreply.github.com>
2023-05-08 10:34:14 +0900

    05.08
    first

---
+++ 0508/메모장.c
@@ -0,0 +1,11 @@
+// HTML
+// h1=큰글자 사용형식:<h1>(글자)</h1>
+// p=일반글자 사용형식:<p style="원하는 스타일">(글자)</p>
+// img=이미지 사용형식:<img 
+                //     width="크기px"
+                //     src="(이미지주소)"/>
+// HTML Element=h1,p,img 등
+// Attribute란 HTML Element의 추가속성(width,src 등등)
+//style=font-size(폰트크기),color(글자 색깔)
+
+//HTML과 CSS 연결고리 <link rel="stylesheet" href="(CSS이름)">
